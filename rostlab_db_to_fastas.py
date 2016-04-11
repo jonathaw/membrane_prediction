a1e8d06fcf47ff4763e74eca9b30df0b9d4832c0
jonathaw <jonathan.weinstein2012@gmail.com>
2016-04-11 16:37:41 +0300

    more files...

--- rostlab_db_to_fastas.py
+++
@@ -1,9 +0,0 @@
-f = open('./data_sets/rostlab_db/rostlab_db.txt', 'r')
-cont_split = f.read().split('>')
-for c in cont_split:
-    if len(c.split()) > 3:
-        continue
-    name = c.split()[0].split('|')[0]
-    with open('./data_sets/rostlab_db/fastas/' + name.lower() + '.fasta', 'wr+') as j:
-        j.write('>' + name.lower() + '\n')
-        j.write(c.split()[1] + '\n')
